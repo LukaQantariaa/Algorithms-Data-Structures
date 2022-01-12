c0ba3d18009470edddcb9a000bac6fbec9fc4dd8
LukaKantaria <l_kantaria@cu.edu.ge>
2022-01-13 01:02:11 +0400

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,28 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split('').every((character, index) => {
+    return character === str[str.length - index - 1];
+  })
+}
+
+function reverse(str) {
+  return str.split('').reduce( (reversed, character) =>  character + reversed, '')
+}
 
 module.exports = palindrome;
+
+// Solution 2
+// function palindrome(str) {
+//   return str === reverse(str);
+// }
+
+// Solution 3
+// function palindrome(str) {
+//   if(str.length % 2 == 0) {
+//     return str.slice(0, str.length/2) === reverse( str.slice(str.length/2, str.length) )
+//   } else {
+//     return str.slice(0, Math.round(str.length/2)-1) === reverse( str.slice(Math.round(str.length/2), str.length) )
+//   }
+// }
