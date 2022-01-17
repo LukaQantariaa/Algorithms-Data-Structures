604cd1570facf0b7ad78c9ffc2a0a4560e95e009
LukaQantariaa <l_kantaria@cu.edu.ge>
2022-01-18 01:22:45 +0400

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,22 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let newArray = [];
+    for(let i=0; i<array.length; i=i+size) {
+        newArray.push(array.slice(i,  size + i ))
+    }
+    return newArray
+}
 
 module.exports = chunk;
+
+// Solution 2
+// function chunk(array, size) {
+//     let chunked = [];
+//     for(let num of array) {
+//         if(chunked.length === 0) chunked.push([])
+//         chunked[chunked.length-1].length === size ? chunked.push([num]) : chunked[chunked.length-1].push(num)
+//     }
+//     return chunked;
+// }
